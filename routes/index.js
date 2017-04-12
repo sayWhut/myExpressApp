70cdc8c4104865ba4c19f38c1699135c592639ac
sayWhut <chris.skura@whut.com>
2017-04-11 22:29:52 -0400

    tst3

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  console.log('app.js');
 });
 
 module.exports = router;
